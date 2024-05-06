125efc0a74a23498cadc75c0e5ed96d5c2e1ee7e
Marais Devis <maraisdcdevis@gmail.com>
2024-05-06 16:00:54 +0200

    changed personal info to my personal info, name, etc.

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,15 +15,15 @@ function Home() {
         <div className="container">
           <div className="text-wrapper">
             <h1 data-test-id="pageTitle" className="title">
-              Halo, saya Lutfhi Ramadhan
+              Hello, My name is Marais Devis
             </h1>
             <p className="description">
-              Saya seorang pekerja lepas pengembang web, fotograper, desainer
-              antarmuka pengguna, dan penulis.
+              I am currently learning coding through the PERN UpLeveled
+              Bootcamp.
             </p>
 
             <Link href="/contact" className="cta">
-              Kontak Saya
+              Contact Marais
             </Link>
           </div>
 
